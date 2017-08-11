2e7db0811cf7d6e529e26169dfcefdec79eab1ed
Andrew Scott <atscott01@gmail.com>
2017-08-10 18:08:40 -0700

    add back srcset

--- src/app/images/progressive-image.component.ts
+++ src/app/images/progressive-image.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'progressive-image',
-  inputs: ['lowQualityImage', 'highQualityImage'],
+  inputs: ['lowQualityImage', 'highQualityImage', 'srcset'],
   templateUrl: './progressive-image.component.html',
   styleUrls: ['./progressive-image.component.css']
 })
 export class ProgressiveImageComponent {
   lowQualityImage: string;
   highQualityImage: string;
+  srcset?: string[];
   highQualityImageLoaded = false;
   lowQualityImageLoaded = false;
 }
